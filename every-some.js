1033df4a9f643a098c1445f0aaed620afcba1925
kevin1547 <c110181143@nkust.edu.tw>
2022-08-28 13:24:03 +0800

    functional-javascript

---
+++ every-some.js
@@ -0,0 +1,6 @@
+function checkUsersValid(goodUsers) {
+    return function allUsersValid(submittedUsers) {
+        return submittedUsers.every(submittedUser => goodUsers.some(goodUser => goodUser.id === submittedUser.id))
+    };
+  }
+  module.exports = checkUsersValid
---
+++ reduce.js
@@ -0,0 +1,13 @@
+function countWords(inputWords) {
+    return inputWords.reduce((accumulator, currentValue) => {
+        box = accumulator.concat(currentValue)
+
+        if(accumulator[currentValue])
+
+
+        return box
+    },[])
+    
+}
+
+module.exports = countWords
